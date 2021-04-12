3b4f82d4a4874d90ad7f759f211f3a617dd04d62
Sebastian Kock <Sebastian.kock@rwth-aachen.de>
2021-04-12 18:39:21 +0200

    initial commit ky003

---
+++ ky003readout/ky003readout.ino
@@ -0,0 +1,28 @@
+/****************************************
+**
+** Author: Sebastian Kock
+**
+** Date: Apr. 12 2021
+**
+** Readout Code for KY-024 Hall Sensor:
+*  Wiring: sensor -> arduino
+*         D0 -> A0
+*         + -> 5V
+*         G -> GND
+*         A0 -> -
+*/
+
+int hallpin = 12;
+int value;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  value = digitalRead(hallpin);
+  Serial.println(value);
+}
+  
